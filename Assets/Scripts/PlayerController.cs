00156cc1137343dff6bb4ebfcc9c3412b8ae44b1
Blob28895 <blob28895@gmail.com>
2025-07-30 19:56:16 -0500

    skeleton player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public HealthSO health;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	public int speed = 5;
+	private bool canMove = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// variables for player movement
+	private Rigidbody2D rb;
+	private ContactFilter2D movementFilter;
+	private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+	private Vector2 lastDirectionFacing = Vector2.zero;
+
+	//private AudioManager audioManager;
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		//audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+
+		//nextLevelName = getNextLevel(SceneManager.GetActiveScene().name);
+	}
+
+	private void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+		if (canMove)
+		{
+			Move();
+		}
+	}
+
+	private void Move()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized;
+
+		bool success = movePlayer(movement);
+		if (!success)
+		{
+			success = movePlayer(new Vector2(movement.x, 0));
+			if (!success)
+			{
+				movePlayer(new Vector2(0, movement.y));
+			}
+		}
+
+		// make player face the last direction they were facing
+		if (movement == Vector2.zero) { movement = lastDirectionFacing; }
+		else { lastDirectionFacing = movement; }
+
+	}
+
+	private bool movePlayer(Vector2 vec)
+	{
+		int count = rb.Cast(
+		vec, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+		movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+		castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+		speed * Time.fixedDeltaTime); // The amount to cast equal to the movement
+
+		if (count == 0)
+		{
+			rb.MovePosition(rb.position + vec * speed * Time.fixedDeltaTime);
+			return true;
+		}
+		return false;
+	}
+
+	public void setCanMove(bool b)
+	{
+		canMove = b;
+	}
+
+
 }
