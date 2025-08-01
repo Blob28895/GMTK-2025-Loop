2bc6b8e1d83e6cb3358ec8a10bdc00ae705a91ac
Blob28895 <blob28895@gmail.com>
2025-07-31 21:39:51 -0500

    added sheep, moved around animations, added attack logic

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -1,21 +1,30 @@
+using System;
+using System.Numerics;
 using UnityEngine;
 
 public class Chicken : MonoBehaviour
 {
     private Animator _animator;
     private EnemyController _enemyController;
+	private Transform _targetPosition;
+    private float cooldownTimer = 0f;
 
-    [SerializeField]
+	[SerializeField]
     private float wanderAnimSpeed = 1f;
     [SerializeField]
     private float chaseAnimSpeed = 3f;
+    [SerializeField]
+    private float attackRange = 1.5f;
+    [SerializeField]
+    private float attackCooldown = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _animator = GetComponent<Animator>();
         _animator.SetBool("moving", true);
         _enemyController = GetComponent<EnemyController>();
-    }
+		_targetPosition = GameObject.FindGameObjectWithTag("Player").transform;
+	}
 
     // Update is called once per frame
     void Update()
@@ -29,5 +38,19 @@ public class Chicken : MonoBehaviour
 			_animator.SetFloat("walkAnimSpeed", chaseAnimSpeed);
 		}
 
+		if(UnityEngine.Vector3.Distance(_targetPosition.position, transform.position) < attackRange && cooldownTimer <= 0)
+        {
+            _animator.SetTrigger("Attack");
+            cooldownTimer = attackCooldown;
+        }
+
+	}
+
+	private void FixedUpdate()
+	{
+		if( cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
 	}
 }
