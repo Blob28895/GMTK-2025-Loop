1faca077a5c562812fd97673fb6e105220e5d4ca
Blob28895 <blob28895@gmail.com>
2025-08-01 23:08:13 -0500

    partial additions to attacks

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,22 +6,29 @@ using Unity.VisualScripting;
 
 public class EnemyController : MonoBehaviour
 {
-	public enum MovementState
-	{
-		wandering,
-		chasing
-	}
+    public enum MovementState
+    {
+        wandering,
+        chasing,
+        attacking
+    }
 
-	private Transform _targetPosition;
+    public interface Attacker
+    {
+        void Attack(Transform targetPosition);
+    }
+
+    private Transform _targetPosition;
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
-	private Vector2 _startingPosition;
+    private Vector2 _startingPosition;
     private Vector2 _delta;
-	private Animator _animator;
-	private float cooldownTimer = 0f;
-	public MovementState _movementState { get; set; }
+    private Animator _animator;
+    private float cooldownTimer = 0f;
+    private Attacker _attacker;
+    public MovementState _movementState { get; set; }
 
-	[Header("Damage Settings")]
+    [Header("Damage Settings")]
     [SerializeField] private float _damageFrequency = 1f;
     [SerializeField] private int _damage = 1;
     //[SerializeField] private GameObject _damageEffect = default;
@@ -31,10 +38,10 @@ public class EnemyController : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float chaseSpeed = 1f;
-	[SerializeField] private float chaseAnimSpeed = 3f;
-	[SerializeField] private float wanderSpeed = 1f;
-	[SerializeField] private float wanderAnimSpeed = 1f;
-	[Tooltip("Amount of time that the wander AI waits to change directions")]
+    [SerializeField] private float chaseAnimSpeed = 3f;
+    [SerializeField] private float wanderSpeed = 1f;
+    [SerializeField] private float wanderAnimSpeed = 1f;
+    [Tooltip("Amount of time that the wander AI waits to change directions")]
     [SerializeField] private float wanderDirectionChangeCooldown = 1f;
     [Tooltip("Maximum distance entity will wander from its start point.")]
     [SerializeField] private float wanderDistance = 5f;
@@ -44,8 +51,8 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float aggroRange = 10f;
     [Tooltip("Distance at which an enemy will stop chasing the player, should be longer than the aggro range.")]
     [SerializeField] private float loseAggroRange = 12f;
-	[SerializeField] private float attackRange = 1.5f;
-	[SerializeField] private float attackCooldown = 5f;
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private float attackCooldown = 5f;
 
     //[Header("Audio")]
     // [SerializeField] private AudioSource _passiveNoise;
@@ -56,31 +63,32 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private HealthSO enemyHealth = default;
 
 
-	void Start()
+    void Start()
     {
         _targetPosition = GameObject.FindGameObjectWithTag("Player").transform;
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _delta = transform.position;
         _startingPosition = transform.position;
-		_animator = GetComponent<Animator>();
-		_animator.SetBool("moving", true);
-		// _passiveNoise = GetComponent<AudioSource>();
-	}
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("moving", true);
+        _attacker = GetComponent<Attacker>();
+        // _passiveNoise = GetComponent<AudioSource>();
+    }
 
     private void FixedUpdate()
     {
 
-		if (cooldownTimer > 0) { cooldownTimer -= Time.deltaTime; }
-		
+        if (cooldownTimer > 0) { cooldownTimer -= Time.deltaTime; }
+
         if ( //If we are wandering and the player is in aggro range, or if we are chasing and the player isnt out of aggro range
             _movementState == MovementState.wandering && Vector3.Distance(_targetPosition.position, transform.position) <= aggroRange ||
-			_movementState == MovementState.chasing && Vector3.Distance(_targetPosition.position, transform.position) <= loseAggroRange) 
+            _movementState == MovementState.chasing && Vector3.Distance(_targetPosition.position, transform.position) <= loseAggroRange)
         {
-			Vector3 direction = (_targetPosition.position - transform.position).normalized;
+            Vector3 direction = (_targetPosition.position - transform.position).normalized;
             _movementState = MovementState.chasing;
             move(new Vector2(direction.x, direction.y), chaseSpeed);
-		}
+        }
         else
         {
             wander();
@@ -89,38 +97,40 @@ public class EnemyController : MonoBehaviour
 
     private void wander()
     {
-		_movementState = MovementState.wandering;
-		wanderDirectionChangeCooldown -= Time.deltaTime;
+        _movementState = MovementState.wandering;
+        wanderDirectionChangeCooldown -= Time.deltaTime;
 
-		if (wanderDirectionChangeCooldown <= 0)
-		{
+        if (wanderDirectionChangeCooldown <= 0)
+        {
             Vector3 targetPosition = new Vector3(
                 _startingPosition.x + UnityEngine.Random.Range(-1 * wanderDistance, wanderDistance),
                 _startingPosition.y + UnityEngine.Random.Range(-1 * wanderDistance, wanderDistance), 0);
 
             _delta = (targetPosition - transform.position).normalized;
 
-			wanderDirectionChangeCooldown = UnityEngine.Random.Range(1f, 5f);
-		}
+            wanderDirectionChangeCooldown = UnityEngine.Random.Range(1f, 5f);
+        }
         move(new Vector2(_delta.x, _delta.y), wanderSpeed);
-	}
+    }
 
-	private void move(Vector2 endposition, float speed)
-	{
+    private void move(Vector2 endposition, float speed)
+    {
 
         _rb.MovePosition(_rb.position + endposition * speed * Time.deltaTime);
-		if (endposition.x > 0)
-		{
+        if (endposition.x > 0)
+        {
             _spriteRenderer.flipX = false;
-		}
+
+        }
         else
         {
             _spriteRenderer.flipX = true;
+
         }
-	}
+    }
 
 
-	private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -141,31 +151,35 @@ public class EnemyController : MonoBehaviour
         //_damageEffect.SetActive(false);
     }
 
-	public void Update()
-	{
-		if (_movementState == MovementState.wandering)
-		{
-			_animator.SetFloat("walkAnimSpeed", wanderAnimSpeed);
-		}
-		else if (_movementState == MovementState.chasing)
-		{
-			_animator.SetFloat("walkAnimSpeed", chaseAnimSpeed);
-		}
+    public void Update()
+    {
+        if (_movementState == MovementState.wandering)
+        {
+            _animator.SetFloat("walkAnimSpeed", wanderAnimSpeed);
+        }
+        else if (_movementState == MovementState.chasing)
+        {
+            _animator.SetFloat("walkAnimSpeed", chaseAnimSpeed);
+        }
 
-        Debug.Log(gameObject.name + " : " + cooldownTimer);
-		if (Vector3.Distance(_targetPosition.position, transform.position) < attackRange && cooldownTimer <= 0)
-		{
-			_animator.SetTrigger("Attack");
-			cooldownTimer = attackCooldown;
-		}
-	}
+        if (Vector3.Distance(_targetPosition.position, transform.position) < attackRange && cooldownTimer <= 0)
+        {
+            _animator.SetTrigger("Attack");
+            cooldownTimer = attackCooldown;
+        }
+    }
 
     public void Damage(int damageAmount)
     {
         enemyHealth.Damage(damageAmount);
-        if(enemyHealth.isDead())
+        if (enemyHealth.isDead())
         {
             Destroy(gameObject);
         }
+    }
+
+    public void attack() 
+    {
+		_attacker.Attack(_targetPosition);
     }
 }
