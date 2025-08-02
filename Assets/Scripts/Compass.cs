8e45e6c735b22bf05a809c0c090556de49b6e324
Blob28895 <blob28895@gmail.com>
2025-08-02 14:19:58 -0500

    added compass

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Compass : MonoBehaviour
+{
+    public Transform _playerLocation;
+    public Transform _farmLocation;
+
+    private RectTransform _needleTransform;
+    private Vector2 directionVector;
+	private void Start()
+	{
+        _playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+        _farmLocation = GameObject.FindGameObjectWithTag("Farm").transform;
+        _needleTransform = GetComponent<RectTransform>();
+	}
+	// Update is called once per frame
+	void Update()
+    {
+        directionVector = (_farmLocation.position - _playerLocation.position).normalized;
+        float angle = Mathf.Atan2(directionVector.y, directionVector.x) * Mathf.Rad2Deg;
+        angle -= 90f;
+        Debug.Log(angle);
+        _needleTransform.eulerAngles = new Vector3(0, 0, angle);
+        ChangeFarmDirection();
+    }
+
+
+    public void ChangeFarmDirection()
+    {
+
+    }
+}
